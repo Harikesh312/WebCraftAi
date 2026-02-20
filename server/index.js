f82a9efa344ab4fc1ca63c952c9028d590cf7a5d
Harikesh312 <kashyapharikesh412@gmail.com>
2026-02-20 22:17:37 +0530

    created login routes and controller and also frontend home page

--- server/index.js
+++ server/index.js
@@ -1,6 +1,21 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+import authRouter from "./routes/auth.routes.js";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+dotenv.config()
 const app = express();
+const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(cookieParser());
+app.use(cors({
+    origin:"http:localhost:5173",
+    credentials:true
+}))
+app.use("/api/auth",authRouter);
 
-app.listen(3000, () => {
-    
-})
+app.listen(port, () => {
+    console.log(`server running to ${port} port`);
+    connectDB();
+});
