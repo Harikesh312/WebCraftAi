ea9df724c3b43ecd25b5a1921dee62e9d8b70504
Harikesh312 <kashyapharikesh412@gmail.com>
2026-05-28 14:27:47 +0530

    make it responsive and add all website listing on dashboard page

--- client/src/pages/WebsiteEditor.jsx
+++ client/src/pages/WebsiteEditor.jsx
@@ -2,9 +2,17 @@ import React, { useEffect, useRef, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { serverUrl } from "../App";
-import { Code, Code2, Monitor, Rocket, Send, X } from "lucide-react";
+import {
+  Code,
+  Code2,
+  MessageSquare,
+  Monitor,
+  Rocket,
+  Send,
+  X,
+} from "lucide-react";
 import { AnimatePresence, motion } from "framer-motion";
-import Editor from '@monaco-editor/react';
+import Editor from "@monaco-editor/react";
 
 const WebsiteEditor = () => {
   const { id } = useParams();
@@ -18,6 +26,7 @@ const WebsiteEditor = () => {
   const [thinkingIndex, setThinkingIndex] = useState(0);
   const [showCode, setShowCode] = useState(false);
   const [showFullPreview, setShowFullPreview] = useState(false);
+  const [showChat, setShowChat] = useState(false);
   const thinkingSteps = [
     "Understanding your request...",
     "Planning layout changes...",
@@ -161,10 +170,13 @@ const WebsiteEditor = () => {
               <Rocket size={14} />
               Deploy
             </button>
+            <button className="p-2 lg:hidden" onClick={() => setShowChat(true)}>
+              <MessageSquare size={18} />
+            </button>
             <button className="p-2" onClick={() => setShowCode(true)}>
               <Code2 className="cursor-pointer" size={18} />
             </button>
-            <button className="p-2" onClick={()=>setShowFullPreview(true)}>
+            <button className="p-2" onClick={() => setShowFullPreview(true)}>
               <Monitor className="cursor-pointer" size={18} />
             </button>
           </div>
@@ -172,6 +184,64 @@ const WebsiteEditor = () => {
 
         <iframe ref={iframeRef} className="flex-1 w-full bg-white" />
       </div>
+
+      <AnimatePresence>
+        {showChat && (
+          <motion.div
+            initial={{ y: "100%" }}
+            animate={{ y: 0 }}
+            exit={{ y: "100%" }}
+            className="fixed inset-0 z-[9999] bg-black flex flex-col"
+          >
+            <Header onclose={()=>setShowChat(false)}/>
+            <>
+              <div className="flex-1 overflow-y-auto px-4 py-4 space-y-4">
+                {messages.map((m, i) => (
+                  <div
+                    key={i}
+                    className={`max-w-[85%] ${m.role === "user" ? "ml-auto" : "mr-auto"}`}
+                  >
+                    <div
+                      className={`px-8 py-2.5 rounded-2xl text-sm leading-relaxed 
+            ${m.role === "user" ? "bg-white text-black" : "bg-white/5 border border-white/10 text-zinc-200"}
+            `}
+                    >
+                      {m.content}
+                    </div>
+                  </div>
+                ))}
+
+                {updateLoading && (
+                  <div className="max-w-[85%] mr-auto ">
+                    <div className="px-4 py-2 rounded-2xl text-xs bg-white/5 border border-white/10 text-zinc-400 italic">
+                      {thinkingSteps[thinkingIndex]}
+                    </div>
+                  </div>
+                )}
+              </div>
+
+              <div className="p-3 border-t border-white/10 ">
+                <div className="flex gap-2">
+                  <textarea
+                    rows="1"
+                    placeholder="Describe Changes..."
+                    className="flex-1 resize-none rounded-2xl px-4 py-3 bg-white/5 border border-white/10 text-sm outline-none"
+                    onChange={(e) => setPrompt(e.target.value)}
+                    value={prompt}
+                  ></textarea>
+                  <button
+                    className="px-4 py-3 rounded-2xl bg-white text-black cursor-pointer"
+                    disabled={updateLoading}
+                    onClick={handleUpdate}
+                  >
+                    <Send size={14} />
+                  </button>
+                </div>
+              </div>
+            </>
+          </motion.div>
+        )}
+      </AnimatePresence>
 
       <AnimatePresence>
         {showCode && (
@@ -183,36 +253,44 @@ const WebsiteEditor = () => {
           >
             <div className="h-12 px-4 flex justify-between items-center border-b border-white/10 bg-[#1e1e1e]">
               <span className="text-sm font-medium">index.html</span>
-              <button onClick={()=>setShowCode(false)} className="cursor-pointer"><X size={18}/></button>
+              <button
+                onClick={() => setShowCode(false)}
+                className="cursor-pointer"
+              >
+                <X size={18} />
+              </button>
             </div>
             <Editor
-            theme='vs-dark'
-            value={code}
-            language="html"
-            onChange={(v)=>setCode(v)}
+              theme="vs-dark"
+              value={code}
+              language="html"
+              onChange={(v) => setCode(v)}
             />
           </motion.div>
-          
         )}
       </AnimatePresence>
 
       <AnimatePresence>
         {showFullPreview && (
-          <motion.div
-          className="fixed inset-0 z-[9999] bg-black"
-          >
-            <iframe className="w-full h-full bg-white" srcDoc={code}/>
-            <button className="absolute top-4 right-4 p-2 bg-black/70 rounded-lg cursor-pointer" onClick={()=>setShowFullPreview(false)}><X/></button>
+          <motion.div className="fixed inset-0 z-[9999] bg-black">
+            <iframe className="w-full h-full bg-white" srcDoc={code} />
+            <button
+              className="absolute top-4 right-4 p-2 bg-black/70 rounded-lg cursor-pointer"
+              onClick={() => setShowFullPreview(false)}
+            >
+              <X />
+            </button>
           </motion.div>
         )}
       </AnimatePresence>
-    </div>
+    </div>   
   );
 
-  function Header() {
+  function Header({onclose}) {
     return (
       <div className="h-14 px-4 flex items-center justify-between border-b border-white/10">
         <span className="font-semibold truncate">{website.title}</span>
+        {onclose && <button onClick={onclose}><X size={18} color="white"/></button>}  
       </div>
     );
   }
